2d32f50998505b1a67dc5d6f0bb66b058675d774
Mateusz <>
2022-04-15 13:43:43 +0200

    refactor(database): switch from sqlite to postgres database

--- cookery/util/database.py
+++ cookery/util/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///cookery/data/database.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///cookery/data/database.db'
+POSTGRESQL_DATABASE_URL = f"postgresql+psycopg2://{os.environ['postgres_user']}:{os.environ['postgres_password']}@apiberentm.hopto.org/cookery"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                       connect_args={"check_same_thread": False},)
+engine = create_engine(
+    POSTGRESQL_DATABASE_URL,
+    # connect_args={"check_same_thread": False},
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -18,4 +22,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
